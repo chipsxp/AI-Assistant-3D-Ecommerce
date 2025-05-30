8e774f2405234955cdc68eafaf83dd8c0f491dc0
chipsxp <chips_xp@yahoo.com>
2025-05-30 14:13:37 -0400

    PORT as a varible

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+const port = process.env.PORT;
 
 app.use("/api/v1/dalle", dalleRoutes);
 
@@ -16,4 +17,5 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: "Hello from DALL.E" })
 });
 
-app.listen(8080, () => console.log('Server has started on port 8080'));
+app.listen(port, () => {console.log('Server has started on port:', port);
+});
