e61ad4fc204ee38a58923b0263b923e052334267
chipsxp <chips_xp@yahoo.com>
2025-05-30 17:04:51 -0400

    client api route

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://ai-assistant-3d-ecommerce-production.up.railway.app",
+    backendUrl: "3dcommerce-server.up.railway.app/api/v1/dalle",
   },
 };
 
