08a5b843e1b3e46df715e0e932b42f5c65c553ea
chipsxp <chips_xp@yahoo.com>
2025-05-30 13:28:01 -0400

    Update Title, text size and color. Got rid od nodemon for web server distribution. Next, Customizer text colors and buttons

--- client/src/config/motion.js
+++ client/src/config/motion.js
@@ -12,7 +12,7 @@ export const slideAnimation = (direction) => {
       x: 0,
       y: 0,
       opacity: 1,
-      transition: { ...transition, delay: 0 },
+      transition: { ...transition, delay: 1 },
     },
     exit: {
       x: direction === "left" ? -100 : direction === "right" ? 100 : 0,
@@ -25,7 +25,7 @@ export const slideAnimation = (direction) => {
 export const fadeAnimation = {
   initial: {
     opacity: 0,
-    transition: { ...transition, delay: 0.5 },
+    transition: { ...transition, delay: 0 },
   },
   animate: {
     opacity: 1,
@@ -45,7 +45,7 @@ export const headTextAnimation = {
     damping: 5,
     stiffness: 40,
     restDelta: 0.001,
-    duration: 0.3,
+    duration: 1.5,
   },
 };
 
@@ -57,14 +57,14 @@ export const headContentAnimation = {
     damping: 7,
     stiffness: 30,
     restDelta: 0.001,
-    duration: 0.6,
-    delay: 0.2,
+    duration: 1.6,
+    delay: 1.5,
     delayChildren: 0.2,
   },
 };
 
 export const headContainerAnimation = {
-  initial: { x: -100, opacity: 0, transition: { ...transition, delay: 0.5 } },
-  animate: { x: 0, opacity: 1, transition: { ...transition, delay: 0 } },
-  exit: { x: -100, opacity: 0, transition: { ...transition, delay: 0 } },
+  initial: { x: -100, opacity: 0, transition: { ...transition, delay: 0 } },
+  animate: { x: 0, opacity: 1, transition: { ...transition, delay: 0.5 } },
+  exit: { x: -100, opacity: 0, transition: { ...transition, delay: 0.5 } },
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,21 +21,21 @@ const Home = () => {
             <img 
               src='./threejs.png'
               alt="logo"
-              className="w-8 h-8 object-contain"
+              className="w-10 h-10 xl:mb-6 mb-4 object-contain"
             />
           </motion.header>
 
           <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
-                LET'S <br className="xl:block hidden" /> DO IT.
+                Big Easy Savings <br className="xl:block hidden" />Custom Shirts
               </h1>
             </motion.div>
             <motion.div
               {...headContentAnimation}
               className="flex flex-col gap-5"
             >
-              <p className="max-w-md font-normal text-gray-600 text-base">
+              <p className="max-w-md font-normal text-blue-400 text-base">
               Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
               </p>
 
