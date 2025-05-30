a94f6ecb4a2426abf9d86ec0f59c59e99aacbd7a
Jimmy Burns (pluckCode) <chips_xp@yahoo.com>
2025-05-30 00:18:26 -0400

    Update config.js
    for Railway host

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://apigee-postman.com",
+    backendUrl: "https://ai-assistant-3d-ecommerce-production.up.railway.app",
   },
 };
 
